76e9f685b654b9bd79b4f53c8348d81098e670b1
Sergii <serge.zp.ua@gmail.com>
2017-10-01 11:44:53 +0200

    Add dissasembling of method

---
+++ DissassembleMethod/DissassembleMethod/DissassemblingExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace DissassembleMethod
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using Mono.Reflection;
+
+    public static class DissassemblingExtensions
+    {
+        public static string ToIlCode<TDelegate>(this Expression<TDelegate> @delegate)
+        {
+            var call = @delegate.Body as MethodCallExpression;
+            if (call == null)
+            {
+                throw new ArgumentException($"{nameof(@delegate)} should be a method call");
+            }
+
+            return call.Method.ToIlCode();
+        }
+
+        public static string ToIlCode(this MethodBase method) =>
+            string.Join(Environment.NewLine, method.GetInstructions());
+    }
+}
---
+++ DissassembleMethod/DissassembleMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DissassembleMethod
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using static System.Console;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Action> someMethod = () => Main(null);
+            string ilCode = someMethod.ToIlCode();
+            string currentMethodIlCode = MethodBase.GetCurrentMethod().ToIlCode();
+
+            WriteLine(ilCode);
+            WriteLine($"Decompiled Il code is {(ilCode == currentMethodIlCode ? "the same": "not the same")} as Il code of current method");
+            ReadKey();
+        }
+    }
+}
