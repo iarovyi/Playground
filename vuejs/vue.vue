88af1887f998debe9bff9a60876df8ad9cd3aa2f
Sergii <serge.zp.ua@gmail.com>
2018-01-01 16:31:43 +0100

    add chrome extension link

--- vuejs/vue.vue
+++ vuejs/vue.vue
@@ -6,31 +6,33 @@ INSTALL
    npm install -g vue-cli   #https://github.com/vuejs/vue-cli
 5) install сервер для случая когда мы используем simple template
    npm install -g live-server
+6) install vuejs chrome extension
+    https://chrome.google.com/webstore/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd?hl=en
 
 GENERATE WEBSITE
-6) сгенерировать шаблон  
+7) сгенерировать шаблон  
     vue init simple <MY PROJECT NAME>          - без webpack
 	vue init webpack-simple <MY PROJECT NAME>  - простой с webpack
 	vue init webpack <MY PROJECT NAME>         - сложный с webpack
-7) сделать текущей папку проекта cd .\<MY PROJECT NAME>
-8) скачать зависомости для сгенерированого шаблона
+8) сделать текущей папку проекта cd .\<MY PROJECT NAME>
+9) скачать зависомости для сгенерированого шаблона
    npm install
    
 RUN
-9) сделать текущей папку проекта cd .\путь к папке
-10) запустить сайт
-    10.1) в случае webpack
+10) сделать текущей папку проекта cd .\путь к папке
+11) запустить сайт
+    11.1) в случае webpack
 	     npm run dev
 	
-	10.2) в случае simple     (https://github.com/vuejs-templates/simple)
+	11.2) в случае simple     (https://github.com/vuejs-templates/simple)
 	     live-server
 	
 	
 DEPLOY
-11) создать продакшн версию
-    11.1) в случае webpack
+12) создать продакшн версию
+    12.1) в случае webpack
 	       npm run build
-	11.2) в случае simple 
+	12.2) в случае simple 
 	      ничего не надо делать, все файлы готовы
 -------------------------------------------------------------------
 
