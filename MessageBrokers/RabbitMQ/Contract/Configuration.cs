2ca14768c4afb98ada9bd043096707e98b8deaf0
Sergii Iarovyi <serge.zp.ua@gmail.com>
2018-03-15 11:58:24 +0100

    RabbitMQ

---
+++ MessageBrokers/RabbitMQ/Contract/Configuration.cs
@@ -0,0 +1,16 @@
+﻿namespace Contract
+{
+    using RabbitMQ.Client;
+
+    public static class Configuration
+    {
+        public static void RegisterQueues(IModel channel)
+        {
+            channel.QueueDeclare(queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+        }
+    }
+}
