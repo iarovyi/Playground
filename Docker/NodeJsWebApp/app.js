fdba4b14283db213841799b9145402a2dcb570b5
Sergii Iarovyi <serge.zp.ua@gmail.com>
2018-06-12 08:06:16 +0200

    Update app.js

--- Docker/NodeJsWebApp/app.js
+++ Docker/NodeJsWebApp/app.js
@@ -1,6 +1,17 @@
-const express = require('express')
-const app = express()
+// Sample node.js web app for Pluralsight Docker CI course
+// For demonstration purposes only
+'use strict';
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var express = require('express'),
+    app = express();
 
-app.listen(8080, () => console.log('Example app listening on port 8080!'))
+app.set('views', 'views');
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+    res.render('home', {
+  });
+});
+
+app.listen(8080);
+module.exports.getApp = app;
