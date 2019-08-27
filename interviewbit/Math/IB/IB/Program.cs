be95dd102ebc0b70b048cad8aca90f05c41bf5d3
Sergii Iarovyi <iarovyi.s@gmail.com>
2019-08-27 08:15:20 +0200

    Add factors

---
+++ interviewbit/Math/IB/IB/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace IB
+{
+    using System;
+    using Math;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetPrimeNumbers.Test();
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
